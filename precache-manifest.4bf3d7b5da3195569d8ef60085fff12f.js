9f2308bcba387bdba12eb37e1dd0ad7a15829053
Mariana Mohr <marianamohr@gmail.com>
2021-06-28 19:00:39 -0300

    Updates

---
+++ precache-manifest.4bf3d7b5da3195569d8ef60085fff12f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0147ec2f73eae7b239a078005de04c0b",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "fb0408a902def4dcd6e1",
+    "url": "/trivia/static/css/main.6916a519.chunk.css"
+  },
+  {
+    "revision": "ad89aecb8cd3cf328a46",
+    "url": "/trivia/static/js/2.e7d7c9af.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia/static/js/2.e7d7c9af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb0408a902def4dcd6e1",
+    "url": "/trivia/static/js/main.a7789173.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "096a202ba60806f154143b2170a08cc6",
+    "url": "/trivia/static/media/hades.096a202b.jpg"
+  },
+  {
+    "revision": "61b88b29b0daa634391eb80bbe09f230",
+    "url": "/trivia/static/media/hera.61b88b29.jpg"
+  },
+  {
+    "revision": "ac1fcfc500abeec5a6fe41f80857a0f0",
+    "url": "/trivia/static/media/poseidon.ac1fcfc5.jpg"
+  },
+  {
+    "revision": "04762dd9b3593fdd78a1d06dd15d936f",
+    "url": "/trivia/static/media/zeus.04762dd9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4543732fa5880fb87093cd6b1dc2713f.js"
+  "/trivia/precache-manifest.4bf3d7b5da3195569d8ef60085fff12f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
